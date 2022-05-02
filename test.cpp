c0064762c4b7fd8d382ffb21999891d6fe420ac9
addie <adi.hladik@gmail.com>
2022-05-02 09:35:19 +0100

    tester update

--- test.cpp
+++ test.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <bitset>
 #include <chrono>
+#include <cstdlib>
+
 #include "bdd.hpp"
+
+#define RANDBOOL (rand() % 2 == 1)
 
 #define MEASURE(tag, code) \
   { \
@@ -15,36 +19,85 @@
               << endl; \
   }
 
-// Formula - !A!L!YD+!UZ!M!SA+!EYCGK+P!NIXF+ZIL!KX
-// poradie - ACDEFGIKLMNPSUWXYZ 
-int main(int argc, char** argv) {
-  string expression = "AB+CD+EF+GH";
-  string order = "ABCDEFGH";
-
-  BDD bdd;
-  MEASURE("create", {
-    bdd.create(expression, order);
-  });
-
-  // Generating all possible combinations of input
-  size_t orderLength = order.length();
-  size_t rowsCount = EXP2(orderLength);
-  
-  vector<string> combinations;
-  string out = "";
-
-  for (size_t n = 0; n < rowsCount; n++) {
-    string bin = bitset<128>(n).to_string();
-    combinations.push_back(bin.substr(128 - orderLength));
+#define MEASURE_SAVE(durationvar, code) \
+  { \
+    auto start = chrono::steady_clock::now(); \
+    code; \
+    auto end = chrono::steady_clock::now(); \
+    durationvar = (double) (chrono::duration_cast<chrono::nanoseconds>(end - start).count()) / 1000000000.0; \
   }
-  
-  MEASURE("integrity check; use", {
-    for (auto variant : combinations) {
-      out.push_back(bdd.use(variant));
-    }
-  });
 
-  cout << ((out == bdd.vector) ? "vectors are equal" : "vectors are not equal") << endl;
+string generateOrder(size_t length) {
+  string order = "";
+  for (size_t idx = 0; idx < length; idx++) {
+    order.push_back(idx + 65);
+  }
+  return order;
+}
+
+string generateRandomExpression(string order) {
+  string output = "";  
+
+  for (size_t idx = 0; idx < order.length() - 1; idx++) {
+    if (idx != 0 && RANDBOOL) output += "+";
+    if (RANDBOOL) output += "!";
+    output += order[idx];
+  }
+
+  return output;
+}
+
+
+int main(int argc, char** argv) {
+  if (argc != 3) {
+    cout << "Invalid arguments; ./test <number of variables> <number of repeat cycles>" << endl;
+    return 1;
+  }
+
+  size_t orderLength = atoi(argv[1]);
+  size_t numberOfCycles = atoi(argv[2]);
+
+  string order = generateOrder(orderLength);
+  
+  cout << "expression,createDuration,useDuraion,vectorsEqual" << endl;
+
+  for (size_t count = 0; count < numberOfCycles; count++) {
+    string expression = generateRandomExpression(order);
+    
+    double createDuration = 0;
+    double useDuration = 0;
+
+    BDD bdd;
+
+    MEASURE_SAVE(createDuration, {
+      bdd.create(expression, order);
+    });
+
+    // Generating all possible combinations of input
+    size_t orderLength = order.length();
+    size_t rowsCount = EXP2(orderLength);
+    
+    vector<string> combinations;
+    string out = "";
+
+    for (size_t n = 0; n < rowsCount; n++) {
+      string bin = bitset<128>(n).to_string();
+      combinations.push_back(bin.substr(128 - orderLength));
+    }
+
+    MEASURE_SAVE(useDuration, {
+      for (const auto& variant : combinations) {
+        out.push_back(bdd.use(variant));
+      }
+    });
+
+    cout 
+      << expression << "," 
+      << createDuration << "," 
+      << useDuration << "," 
+      << (out == bdd.vector)
+      << endl;
+  }
 
   return 0;
 }
