e0faa7b51175f091581e08bdc6c7748c193f9368
addie <adi.hladik@gmail.com>
2022-05-03 00:06:31 +0100

    wait added

--- loop.sh
+++ loop.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 find ./measurement/ -name '*.json' -delete
 
-for ((i = 13; i <= 16; i++))
+for ((i = 13; i <= 26; i++))
 do
   echo -e "Testing for" $i
   ./test $i 2 >>./measurement/${i}.10.json &
 done
+wait
