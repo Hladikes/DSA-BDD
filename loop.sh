f8a7dc3da5b76175931c34ceb11634fd820ae667
addie <adi.hladik@gmail.com>
2022-05-03 14:39:41 +0100

    loop.sh is accepting cli args

--- loop.sh
+++ loop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 find ./measurement/ -name '*.json' -delete
 
-for ((i = 13; i <= 26; i++))
+for ((i = $1; i <= $2; i++))
 do
   echo -e "Testing for" $i
-  ./test $i 2 >>./measurement/${i}.10.json &
+  ./test $i $3 >>./measurement/${i}.${3}.json &
 done
 wait
