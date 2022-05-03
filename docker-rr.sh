46e57007b06c31a7aaf49172598a6181adba8c14
addie <adi.hladik@gmail.com>
2022-05-03 15:27:11 +0100

    remove container after exection

--- docker-rr.sh
+++ docker-rr.sh
@@ -6,4 +6,4 @@ IMAGE_ID="$(docker images | grep "bdd" | xargs | cut -d " " -f 3 | xargs)"
 docker rmi $IMAGE_ID
 
 docker build . -t bdd
-docker run -d -e "MIN=$1" -e "MAX=$2" -e "FNCOUNT=$3" -v $(pwd)/measurement:/app/measurement bdd
+docker run -d -e "MIN=$1" -e "MAX=$2" -e "FNCOUNT=$3" --rm -v $(pwd)/measurement:/app/measurement bdd
