ca00a768f2b64eb79b028435662bb84ad7198f03
addie <adi.hladik@gmail.com>
2022-05-03 14:56:09 +0100

    uhhh

--- docker-rr.sh
+++ docker-rr.sh
@@ -5,5 +5,10 @@ docker rm $CONTAINER_ID
 IMAGE_ID="$(docker images | grep "bdd" | xargs | cut -d " " -f 3 | xargs)"
 docker rmi $IMAGE_ID
 
+echo Docker RR
+echo min $1
+echo max $2
+echo fncount $3
+
 docker build . -t bdd
 docker run -d -e "min=$1" -e "max=$2" -e "fncount=$3" -v $(pwd)/measurement:/app/measurement bdd
