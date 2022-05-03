c98c94702d71a1451aec3b3314aa01bc04b8e258
addie <adi.hladik@gmail.com>
2022-05-03 19:54:16 +0100

    distribution decreased

--- src/jhvolka_dnfgen.hpp
+++ src/jhvolka_dnfgen.hpp
@@ -39,7 +39,7 @@ namespace jhvolka {
     // Initialize random device and distribution
     std::random_device        rd;
     std::mt19937          gen(rd());
-    std::uniform_int_distribution<> distrib(0, 6);
+    std::uniform_int_distribution<> distrib(0, 1);
 
     std::stringstream out;
         
