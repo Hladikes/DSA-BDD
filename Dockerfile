ea8630ad7e89c73ca9349a2901faf2d65cc11b91
addie <adi.hladik@gmail.com>
2022-05-06 17:44:33 +0100

    t h r e a d s

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,7 @@ COPY compile.sh compile.sh
 ENV MIN 13
 ENV MAX 26
 ENV FNCOUNT 5
+ENV JOBSCOUNT 1
 
 RUN chmod 777 ./loop.sh
 RUN chmod 777 ./compile.sh
@@ -20,4 +21,4 @@ RUN apt-get -qq -y install zip
 
 RUN ./compile.sh
 
-CMD ./loop.sh $MIN $MAX $FNCOUNT
+CMD ./loop.sh $MIN $MAX $FNCOUNT $JOBSCOUNT
--- loop.sh
+++ loop.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-find ./measurement/ -name '*.json' -delete
+
+# $1 min number of variables
+# $2 max number of variables
+# $3 number of functions per job
+# $4 number of paralel jobs
 
 for ((i = $1; i <= $2; i++))
 do
+  mkdir ./measurement/${i}_vars
+
   echo -e "Testing for" $i
-  ./bin/test $i $3 >>./measurement/${i}.${3}.json &
+  
+  for ((j = 1; j <= $4; j++)) {
+    ./bin/test $i $3 >>./measurement/${i}_vars/${i}v.${3}fn.${j}.json &
+  }
+
+  wait
 done
-wait
-zip -r measurement/measurement_${1}_${2}_${3}.zip measurement/*.json
+
+zip -r measurement/measurement_${1}_${2}_${3}.zip measurement
